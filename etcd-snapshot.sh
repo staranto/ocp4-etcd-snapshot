a7cb220347ccd5f5f1a700b4c663aa0b0d012c3e
Steve Taranto <staranto@gmail.com>
2020-12-17 06:25:59 -0500

    WIP

--- etcd-snapshot.sh
+++ etcd-snapshot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SNAPSHOT_HOST=${1}
-SNAPSHOT_DIR=${2:-/home/core/etcd-snapshots}
+SNAPSHOT_DIR=/tmp/${2:-etcd-snapshots}
 SNAPSHOT_DAYS=${3:-7}
 THIS_HOST=$(hostname -s)
 
@@ -15,12 +15,4 @@ mkdir -p ${SNAPSHOT_DIR}
 
 /usr/local/bin/cluster-backup.sh ${SNAPSHOT_DIR}
 
-
-# 1. If we're not the $SNAPSHOT_HOST then stop without error.
-# 2. mkdir -p $SNAPSHOT_DIR
-# 3. Run cluster_backup.sh
-
-
-# A cronjob pod does this...
-# 4. Find delete stale files
-# 5. Copy of new files.
+find ${SNAPSHOT_DIR} -type f -mtime +${SNAPSHOT_DAYS} -exec rm -f {} \;
