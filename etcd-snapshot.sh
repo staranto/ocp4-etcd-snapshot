3ad5f3e6f4819f5c18159851186788bbc455e27e
Steve Taranto <staranto@users.noreply.github.com>
2021-03-02 07:13:57 -0500

    Helm (#2)
    * First cut of Helm chart.
    
    * Helm repo.
    
    * Remove Helm repo.
    
    * Removed pod dev directory.
    
    * Make StorageClass optional.
    
    * First cut after moving to initContainer scheme.
    
    * Misc testing.
    
    * Intro etcd-snapshot container image.
    
    * Profound changes to image.
    
    * Changed container name.
    
    * Put upgrade guard around PV since it's immutable.
    
    * Misc testing.
    
    * Prep for non-pv sync mode.
    
    * "Fix" file ownership on PV-based snapshot storage.
    
    * MVP for pv mode.
    
    * Cheap, temporary hack at dealing with ssh key pair.
    
    * MVP for sync mode.
    
    * MVP cleanup.
    
    * Marginally less dumb handling of ssh keypair.
    
    * Fixed find -mtime parameter so it selects on proper interval.
    
    * Namespace agnostic.
    
    * First cut repo.
    
    * First cut Helm repo source.
    
    * Test of adding new chart version to repo.
    
    * Don't include package .tgz
    
    * Reintroduce .Values.baseDomain
    
    * Debug mode logic; Deal with --force for OCP 4.7.
    
    * Rsync to master nodes labeled as mirrors.
    
    * Change command line parsing.
    
    * Bump to 1.0.0
    
    * Standard label template.
    
    * Bump to 1.0.1

--- etcd-snapshot.sh
+++ etcd-snapshot.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
 
-SNAPSHOT_HOST=${1}
-SNAPSHOT_DIR=${2:-/home/core/etcd-snapshots}
-SNAPSHOT_DAYS=${3:-0}
+SNAPSHOT_DIR=/home/core/etcd-snapshots
+SNAPSHOT_DAYS=0
 THIS_HOST=$(hostname)
 VALID_DIRS="/home/core /var/home/core /tmp"
 
+while [[ $# -gt 0 ]]; do
+    k="$1"
+    case $k in
+        -f|--force)
+            SNAPSHOT_FORCE="--force"
+            shift
+            ;;
+        -h|--host)
+            SNAPSHOT_HOST="$2"
+            shift
+            shift
+            ;;
+        -o|--output)
+            SNAPSHOT_DIR="$2"
+            shift
+            shift
+            ;;          
+        -p|--purge)
+            SNAPSHOT_DAYS="$2"
+            shift
+            shift
+            ;;
+        *)
+            echo "Unknown argument: $0 $*"
+            exit 1
+    esac
+done
+
 echo "--- Runtime ---"
+echo "Command Line=$0 $*"
 echo "SNAPSHOT_HOST=${SNAPSHOT_HOST}"
 echo "SNAPSHOT_DIR=${SNAPSHOT_DIR}"
 echo "SNAPSHOT_DAYS=${SNAPSHOT_DAYS}"
+echo "SNAPSHOT_FORCE=${SNAPSHOT_FORCE}"
 echo "---------------"
+
+set -x
 
 shopt -s nocasematch
 if [[ "pod-mode" != "${SNAPSHOT_HOST}" ]] && [[ "${THIS_HOST}" != ${SNAPSHOT_HOST}* ]]; then
@@ -32,9 +63,11 @@ fi
 
 mkdir -p ${SNAPSHOT_DIR}
 
-/usr/local/bin/cluster-backup.sh ${SNAPSHOT_DIR}
+/usr/local/bin/cluster-backup.sh ${SNAPSHOT_FORCE} ${SNAPSHOT_DIR}
 
-sleep 3
+chown -R core:core ${SNAPSHOT_DIR}
+
+sleep 1
 
 if [[ "${SNAPSHOT_DAYS}" == "0" ]]; then
     echo "Skipping snapshot purge."
@@ -45,4 +78,4 @@ echo "--- Pre-purge state ---"
 ls -l ${SNAPSHOT_DIR}
 
 echo "--- Purging stale snapshots ---"
-find ${SNAPSHOT_DIR} -type f -mtime +${SNAPSHOT_DAYS} -print -delete
+find ${SNAPSHOT_DIR} -type f -mtime ${SNAPSHOT_DAYS} -print -delete
